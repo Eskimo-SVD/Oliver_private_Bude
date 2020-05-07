02397856ae9e04d321ca6ed9d4f8708de59911e5
Krueger <Oliver.Krueger@focke.de>
2020-05-07 08:55:31 +0200

    Lernen mit dem Compiler

---
+++ test2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#define SECRET 1234
+
+int main(int argc, char **argv)
+{
+  if (HALLO + 1233 == SECRET)
+  {
+    printf("Sie haben die Kokosnuss geknackt!\n");
+  }
+  else
+  {
+    printf("Was auch immer?\n");
+  }
+  return 0;
+}

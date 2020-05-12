115f890dcacf3f5cfe789966ec1a84546737997a
Krueger <Oliver.Krueger@focke.de>
2020-05-12 10:16:16 +0200

    new compile and test - but wrong

--- hello_viewer.cpp
+++ hello_viewer.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main()
 {
-  cout<<"hello Viewers of Oliver Krueger\n";
+    cout<<"hello Viewers of Oliver Krueger\n";
     cout<<"dies ist die zweite Zeile\n";
     cout<<"dies ist die dritte Zeile\n";
     cout<<"unnnndddd hier kommt Zeile 4!!!\n";

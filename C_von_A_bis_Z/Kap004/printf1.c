41e3e8007f2e61f8ded0b4ed00e85730893e5a73
Krueger <Oliver.Krueger@focke.de>
2020-05-12 08:18:49 +0200

    Just another testing in Kap004

--- C_von_A_bis_Z/Kap004/printf1.c
+++ C_von_A_bis_Z/Kap004/printf1.c
@@ -3,7 +3,7 @@
 
 int main(void) {
    int zeichen;
-   zeichen=printf("Hallo Welt written by Oliver for testing");
+   zeichen=printf("Hallo Welt written by Oliver for testing\n Extended for another test.");
    printf("\n\n enthaelt %d Zeichen\n",zeichen);  //10 Zeichen
    return 0;
 }

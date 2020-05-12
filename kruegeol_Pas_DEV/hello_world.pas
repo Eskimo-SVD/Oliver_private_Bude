06a35b9c992d8d4ee63e34de108285865d8144f3
Krueger <Oliver.Krueger@focke.de>
2020-05-12 21:03:56 +0200

    Having worked with PASCAL.

--- kruegeol_Pas_DEV/hello_world.pas
+++ kruegeol_Pas_DEV/hello_world.pas
@@ -2,4 +2,10 @@ program Hello;
 begin
   writeln ('Hello, world.');
   readln;
+  writeln ('Hello, world, too.');
+  readln;
+  writeln ('Hello, world, three.');
+  readln;
+  writeln (';-).................');
+  readln;
 end.

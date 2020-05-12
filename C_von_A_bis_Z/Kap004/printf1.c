0741cbb47e1c3b854d93029be63c1c1d66f5d2f2
Krueger <Oliver.Krueger@focke.de>
2020-05-12 08:12:20 +0200

    Kap004 third compile for learning

--- C_von_A_bis_Z/Kap004/printf1.c
+++ C_von_A_bis_Z/Kap004/printf1.c
@@ -3,7 +3,7 @@
 
 int main(void) {
    int zeichen;
-   zeichen=printf("Hallo Welt");
-   printf(" enthaelt %d Zeichen\n",zeichen);  //10 Zeichen
+   zeichen=printf("Hallo Welt written by Oliver for testing");
+   printf("\n\n enthaelt %d Zeichen\n",zeichen);  //10 Zeichen
    return 0;
 }

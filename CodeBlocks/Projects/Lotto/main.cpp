8e5e64d7bde79a5d61dbbe6a5bfb7cc6a18d73a9
Krueger <Oliver.Krueger@focke.de>
2020-05-25 11:48:48 +0200

    Zusatzzahl  - aber noch weiter verbessern

--- CodeBlocks/Projects/Lotto/main.cpp
+++ CodeBlocks/Projects/Lotto/main.cpp
@@ -1,3 +1,5 @@
+/* 20200525 - Berechnung der Zusatzzahl soll noch besser eingebaut werden */
+
 #include <iostream>   // die Bildschirmausgabe
 #include <cstdlib>    // die Zufallsfunktionen
 using namespace std;
@@ -25,4 +27,22 @@ int main()
         } while (!neueZahl);
         cout << lotto[i] << endl;
     }
+
+    for(i=0; i<1; i++) // ziehe die Zusatzzahl
+    {
+        do  // wiederhole die Ziehung, bis die neue Zahl
+        {   // nicht mit einer der vorigen identisch ist.
+            lotto[i] = rand() % 49 + 1;
+            neueZahl = true; // positive Grundeinstellung
+            for (j=0; j<i; j++)
+            {  // durchlaufe alle bisher gezogenen Kugeln
+                if (lotto[j]==lotto[i])
+                { // Hier wurde ein Doppelter entdeckt
+                    neueZahl = false;
+                }
+            }
+        } while (!neueZahl);
+        cout << "Zusatzzahl : "<< lotto[i] << endl;
+    }
+
 }

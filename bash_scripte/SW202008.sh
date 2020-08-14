9b0cf48d4ca45db5aa3e7d4e30bce52409e1bc2b
Oliver Krüger <Eskimo.SVD@googlemail.com>
2020-08-14 12:13:08 +0200

    20200814 - new Software - script

--- bash_scripte/SW202008.sh
+++ bash_scripte/SW202008.sh
@@ -10,7 +10,10 @@ sudo pip3 install dropbox
 
 sudo apt install raspi -gpio
 
+python3 -m pip install -U pip
+python3 -m pip install -U matplotlib
 
+sudo apt install python3 -pandas
 
 sudo apt full-upgrade
 

bcb07a38ebd4f2bee52c35d18111513bedeb56e6
Oliver Krüger <Eskimo.SVD@googlemail.com>
2020-08-15 21:47:03 +0200

    20200815 - Wolfram_Language - bash_scripte

---
+++ RaspberryPI_Handbuch/bash_excamples/exp_1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+read var_year
+echo "The year is: $var_year"
+
+echo -n "Enter your name and press [ENTER]: "
+read var_name
+echo "Your name is: $var_name"
+
+echo -n "Press [ENTER] to continue,...: "
+read var_name
+echo "You can go on!...."
--- bash_scripte/SW202008.sh
+++ bash_scripte/SW202008.sh
@@ -2,8 +2,10 @@
 
 sudo apt update
 sudo apt install thonny
+read -n 1 -s
 
 sudo apt install python -picamera python3 -picamera
+read -p "Press any key to continue... " -n1 -s
 
 sudo apt install python3 -pip
 sudo pip3 install dropbox
@@ -14,6 +16,11 @@ python3 -m pip install -U pip
 python3 -m pip install -U matplotlib
 
 sudo apt install python3 -pandas
+
+echo "https://airspayce.com/mikem/bcm2835/ Install"
+read -n 1 -s
+
+sudo apt install ghostscript
 
 sudo apt full-upgrade
 

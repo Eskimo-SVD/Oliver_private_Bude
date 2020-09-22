0feeaa0b8ff809dea6df05bf3ec1747756af12f9
Oliver Krüger (Eskimo_SVD) <ok@oliver-krueger.de>
2020-09-22 13:41:43 +0200

    20200922-1-alter.cpp

---
+++ c++ Snipes/alter.cpp
@@ -0,0 +1,17 @@
+// Quellcode in c++ Alter
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *atgv[])
+{
+  int alter; //Variable vom Typ Integer
+
+  cout << "Wie alt bist Du?";
+  cin >> alter;
+  cout << "Du bist " << alter << " Jahre alt." << endl;
+  getc();
+  return 0;
+}
